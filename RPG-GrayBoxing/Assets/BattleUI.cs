4e7dd5c87c469418e1b122950ef48a38edd20588
elvis0604 <46872849+elvis0604@users.noreply.github.com>
2019-12-03 03:33:21 -0600

    Added battle UI

---
+++ RPG-GrayBoxing/Assets/BattleUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUI : MonoBehaviour
+{
+    public GameObject battle_UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        battle_UI.SetActive(!battle_UI.activeSelf);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
